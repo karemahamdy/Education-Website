8f533d10630fa71a92914b9923f7b57613ef9cb7
karema-hamdy <https://github.com/karemahamdy>
2023-11-28 21:38:59 +0200

    about & courses

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ span.onclick = function () {
     behavior: "smooth",
   });
 };
-// 
+// show answer of questions
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus';
+        } else {
+            icon.className = 'fa-solid fa-plus';
+        }
+    })
+})
