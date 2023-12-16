3a527b60a3180417520e5bc42c6a1b8e358ad8b7
karema-hamdy <https://github.com/karemahamdy>
2023-12-16 07:43:22 +0200

    edit question amd courses sections

--- index.js
+++ index.js
@@ -11,20 +11,20 @@
 //   });
 // };
 // // show answer of questions
-// const faqs = document.querySelectorAll('.faq');
-// faqs.forEach(faq => {
-//     faq.addEventListener('click', () =>{
-//         faq.classList.toggle('open');
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
 
-//         // change icon
-//         const icon = faq.querySelector('.faq_icon i');
-//         if (icon.className === 'fa-solid fa-plus'){
-//             icon.className = 'fa-solid fa-minus';
-//         } else {
-//             icon.className = 'fa-solid fa-plus';
-//         }
-//     })
-// })
+        // change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className === 'fa-solid fa-plus'){
+            icon.className = 'fa-solid fa-minus';
+        } else {
+            icon.className = 'fa-solid fa-plus';
+        }
+    })
+})
 
 // counter number in about section
 let number = document.querySelectorAll(".box .num");
